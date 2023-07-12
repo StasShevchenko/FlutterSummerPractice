a040538d96a90802f6d144ea61fb9e30dd69a074
Stas <dark.knight22832@gmail.com>
2023-07-12 10:55:57 +0300

    Icons are added, basic start page layout is created

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_summer_practice/presentation/theme/app_colors.dart';
 import 'package:flutter_summer_practice/presentation/theme/app_theme.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -35,47 +36,85 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    final Widget companyIcon = SvgPicture.asset(
+      'assets/icons/company_icon.svg',
+      width: 35,
+      height: 35,
+    );
+
     return Scaffold(
       backgroundColor: AppColors.background,
       appBar: AppBar(
+        leading: Center(child: companyIcon),
         title: Text(widget.title),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: AppColors.primaryWhite,
+                  borderRadius: BorderRadius.circular(10)
+                ),
+                height: 35,
+                width: 35,
+                child: IconButton(onPressed: (){}, icon: SvgPicture.asset('assets/icons/bell_icon.svg',),),
+              ),
+            ),
+          ),
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-           Row(
-             children: [
-               Text('Статус:', style: Theme.of(context).textTheme.bodyLarge,),
-               const SizedBox(width: 10,),
-               const Expanded(
-                 child: TextField(
-                   enabled: false,
-                   decoration: InputDecoration(
-                     labelText: 'Парковка доступна!'
-                   ),
-                 ),
-               )
-             ],
-           ),
-            SizedBox(height: 20,),
             Row(
               children: [
-                Text('Место:', style: Theme.of(context).textTheme.bodyLarge,),
-                const SizedBox(width: 10,),
+                Text(
+                  'Статус:',
+                  style: Theme.of(context).textTheme.bodyLarge,
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
                 const Expanded(
                   child: TextField(
                     enabled: false,
-                    decoration: InputDecoration(
-                        labelText: '512'
-                    ),
+                    decoration:
+                        InputDecoration(labelText: 'Парковка доступна!'),
                   ),
                 )
               ],
             ),
-            SizedBox(height: 20,),
-            ElevatedButton(onPressed: (){}, child: Text('404'), style: ElevatedButton.styleFrom(minimumSize: Size.fromHeight(50)),)
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                Text(
+                  'Место:',
+                  style: Theme.of(context).textTheme.bodyLarge,
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                const Expanded(
+                  child: TextField(
+                    enabled: false,
+                    decoration: InputDecoration(labelText: '512'),
+                  ),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(minimumSize: const Size.fromHeight(50)),
+              child: const Text('404'),
+            )
           ],
         ),
       ),
